61336aa2ef2d1d3cfbd80d74b0447328d5df4b91
CrowderSoup <aaron@aaroncrowder.com>
2012-09-05 22:15:11 -0600

    Fixed an issue with permissions

--- application/sys/config.php
+++ application/sys/config.php
@@ -4,12 +4,6 @@
      * @author Aaron Crowder <aaron@aaroncrowder.com>
      */
 
-    // Prevent Direct Access to this file
-    if (!defined('BASEPATH')) {
-        header('HTTP/1.0 403 Forbidden');
-        exit;
-    }
-
     /**
      * System Constants
      * 
